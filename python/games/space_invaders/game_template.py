1ce3d99bca08889d8b8a68ce4783c6734e12b0b5
Kyle Carnahan <carnahan.kyle@gmail.com>
2024-06-03 22:51:59 -0400

    second commit - test

---
+++ python/games/space_invaders/game_template.py
@@ -0,0 +1,145 @@
+import pygame, random
+
+pygame.init()
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 400
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Enter the Matrix:")
+
+FPS = 60
+clock = pygame.time.Clock()
+
+PLAYER_STARTING_LIVES = 3
+PLAYER_VELOCITY = 5
+COIN_STARTING_VELOCITY = 5
+COIN_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+GREEN = (0,255,0)
+DARKGREEN = (10,50,10)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+font = pygame.font.SysFont('Lucidasans', 32)
+score_text = font.render("Score: " + str(score),True,GREEN,DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10,10)
+
+title_text = font.render("Feed the Dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH//2
+title_rect.y = 10
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+continue_text = font.render("Press any key to play again", True, GREEN, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+coin_sound = pygame.mixer.Sound("explosion.wav")
+miss_sound = pygame.mixer.Sound("firemissile.wav")
+miss_sound.set_volume(.1)
+pygame.mixer.music.load("background.wav")
+
+#Set images
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT//2
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+coin_rect.y = random.randint(64, WINDOW_HEIGHT)
+
+
+#main game loop
+pygame.mixer.music.play(-1,0.0)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #check if user wants to move
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_rect.top > 64:
+        player_rect.y -= PLAYER_VELOCITY
+    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
+        player_rect.y += PLAYER_VELOCITY
+        
+    #Move the coin
+    if coin_rect.x < 0:
+        #Player missed the coin
+        player_lives -= 1
+        miss_sound.play()
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+    else:
+        coin_rect.x -= coin_velocity
+
+    #check for collisions
+    if player_rect.colliderect(coin_rect):
+        score+=1
+        coin_sound.play()
+        coin_velocity+=COIN_ACCELERATION
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+
+        
+    score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+    lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+
+    #check for gameover
+    if player_lives == 0:
+        display_surface.blit(game_over_text,game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #Pause the game until the player presses a key, then reset the game
+
+        pygame.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                #if the player wants to play again
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+                    player_rect.y = WINDOW_HEIGHT//2
+                    coin_velocity = COIN_STARTING_VELOCITY
+                    pygame.mixer.music.play(-1,0.0)
+                    is_paused = False
+                #if the player wants to quit
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+                    
+    display_surface.fill(BLACK)
+
+    #blit the HUD to screen
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    pygame.draw.line(display_surface, WHITE, (0,64), (WINDOW_WIDTH,64), 2)
+
+    #blit the assets to the screen
+    display_surface.blit(player_image, player_rect)
+    display_surface.blit(coin_image, coin_rect)
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+
+pygame.quit()
