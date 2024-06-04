24dae6c0632d0063a3e62dc6b92f086f4a8ef870
Kyle Carnahan <carnahan.kyle@gmail.com>
2024-06-04 12:39:56 -0400

    Added game elements and wrote player classes

--- python/games/space_invaders/space_invader.py
+++ python/games/space_invaders/space_invader.py
@@ -14,12 +14,133 @@ pygame.display.set_caption("SPACE INVADERS")
 FPS = 60
 clock = pygame.time.Clock()
 
-# Set the defaults
-PLAYER_STARTING_LIVES = 3
+# Define the classes
+class Game():
+    """A class to help control and update the gameplay"""
 
-score = 0
-player_lives = PLAYER_STARTING_LIVES
+    def __init__(self):
+        """Initialize the game"""
+        pass
 
+    def update(self):
+        """Update the game"""
+        pass
+
+    def draw(self):
+        """Draw the HUD and other information to display"""
+        pass
+
+    def shift_aliens(self):
+        """Shift a weave of aliens down the screen and reverse direction"""
+        pass
+
+    def check_collisions(self):
+        """Check for collisions"""
+        pass
+
+    def check_round_complete(self):
+        """Check to see if the round is complete"""
+        pass
+
+    def check_game_status(self):
+        """Check to see the status of the game and how the player died"""
+        pass
+
+    def pause_game(self):
+        """Pause the game"""
+        pass
+
+    def reset_game(self):
+        """Reset the game to beginning"""
+        pass
+
+    def start_new_round(self):
+        """Start a new round"""
+        pass
+
+
+class Player(pygame.sprite.Sprite):
+    """A class to model a spaceship the user can control"""
+
+    def __init__(self, bullet_group):
+        """Initialize the player"""
+        super().__init__()
+        self.image = pygame.image.load("spaceship.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WINDOW_WIDTH//2
+        self.rect.bottom = WINDOW_HEIGHT
+
+        self.lives = 3
+        self.velocity = 8
+        self.bullet_group = bullet_group
+
+        self.shoot_sound = pygame.mixer.Sound("laser_sound.wav")
+
+    def update(self):
+        """Update the player"""
+        pass
+
+    def fire(self):
+        """Update the player"""
+        pass
+
+    def reset(self):
+        """"Fire a laser"""
+        pass
+
+class alien(pygame.sprite.Sprite):
+    """A class to model an enemy alien"""
+
+    def __init__(self):
+        """Initialize the alien"""
+        pass
+
+    def update(self):
+        """Update the alien"""
+        pass
+
+    def fire(self):
+        """Fire a laser"""
+        pass
+
+    def reset(self):
+        """Reset the position"""
+        pass
+
+class PlayerBullet(pygame.sprite.Sprite):
+    """A class to model the bullet fired by the player"""
+
+    def __init__(self):
+        """Initialize the bullet"""
+        pass
+
+    def update(self):
+        """Update the bullet"""
+
+class AlienBullet(pygame.sprite.Sprite):
+    """A class to model the bullet fired by the alien"""
+
+    def __init__(self):
+        """Initialize the bullet"""
+        pass
+
+    def update(self):
+        """Update the bullet"""
+
+# Create the bullet groups
+my_player_bullet_group = pygame.sprite.Group()
+my_alien_bullet_group = pygame.sprite.Group()
+
+# Create a player group
+my_player_group = pygame.sprite.Group()
+my_player = Player(my_player_bullet_group)
+my_player_group.add(my_player)
+
+# Create an alien group. Add alien objects via the game's start new round method
+my_alien_group = pygame.sprite.Group()
+
+# Create a game object
+my_game = Game()
 
 # Run the game
 running = True
@@ -27,6 +148,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    display_surface.fill((0,0,0))
+
+    my_player_group.update()
+    my_player_group.draw(display_surface)
+
+    my_alien_bullet_group.update()
+    my_alien_group.draw(display_surface)
+
+    my_player_bullet_group.update()
+    my_player_bullet_group.draw()
+    my_alien_bullet_group.update()
+    my_alien_bullet_group.draw()
+ 
+    my_game.update()
+    my_game.draw()
 
     #check if user wants to move
     keys = pygame.key.get_pressed()
@@ -38,114 +175,5 @@ while running:
     pygame.display.update()
     clock.tick(FPS)
 
-
+# End the game
 pygame.quit()
-
-
-
-GREEN = (0,255,0)
-DARKGREEN = (10,50,10)
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-
-font = pygame.font.SysFont('Lucidasans', 32)
-score_text = font.render("Score: " + str(score),True,GREEN,DARKGREEN)
-score_rect = score_text.get_rect()
-score_rect.topleft = (10,10)
-
-title_text = font.render("Feed the Dragon", True, GREEN, WHITE)
-title_rect = title_text.get_rect()
-title_rect.centerx = WINDOW_WIDTH//2
-title_rect.y = 10
-
-lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
-lives_rect = lives_text.get_rect()
-lives_rect.topright = (WINDOW_WIDTH - 10, 10)
-
-game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
-game_over_rect = game_over_text.get_rect()
-game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
-
-continue_text = font.render("Press any key to play again", True, GREEN, DARKGREEN)
-continue_rect = continue_text.get_rect()
-continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
-
-coin_sound = pygame.mixer.Sound("explosion.wav")
-miss_sound = pygame.mixer.Sound("firemissile.wav")
-miss_sound.set_volume(.1)
-pygame.mixer.music.load("background.wav")
-
-#Set images
-player_image = pygame.image.load("dragon_right.png")
-player_rect = player_image.get_rect()
-player_rect.left = 32
-player_rect.centery = WINDOW_HEIGHT//2
-
-coin_image = pygame.image.load("coin.png")
-coin_rect = coin_image.get_rect()
-coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
-coin_rect.y = random.randint(64, WINDOW_HEIGHT)
-
-
-#main game loop
-pygame.mixer.music.play(-1,0.0)
-
-        
-    #Move the coin
-    if coin_rect.x < 0:
-        #Player missed the coin
-        player_lives -= 1
-        miss_sound.play()
-        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
-        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
-    else:
-        coin_rect.x -= coin_velocity
-
-    #check for collisions
-    if player_rect.colliderect(coin_rect):
-        score+=1
-        coin_sound.play()
-        coin_velocity+=COIN_ACCELERATION
-        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
-        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
-
-        
-    score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
-    lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
-
-    #check for gameover
-    if player_lives == 0:
-        display_surface.blit(game_over_text,game_over_rect)
-        display_surface.blit(continue_text, continue_rect)
-        pygame.display.update()
-
-        #Pause the game until the player presses a key, then reset the game
-
-        pygame.mixer.music.stop()
-        is_paused = True
-        while is_paused:
-            for event in pygame.event.get():
-                #if the player wants to play again
-                if event.type == pygame.KEYDOWN:
-                    score = 0
-                    player_lives = PLAYER_STARTING_LIVES
-                    player_rect.y = WINDOW_HEIGHT//2
-                    coin_velocity = COIN_STARTING_VELOCITY
-                    pygame.mixer.music.play(-1,0.0)
-                    is_paused = False
-                #if the player wants to quit
-                if event.type == pygame.QUIT:
-                    is_paused = False
-                    running = False
-                    
-    display_surface.fill(BLACK)
-
-    #blit the HUD to screen
-    display_surface.blit(score_text, score_rect)
-    display_surface.blit(title_text, title_rect)
-    display_surface.blit(lives_text, lives_rect)
-    pygame.draw.line(display_surface, WHITE, (0,64), (WINDOW_WIDTH,64), 2)
-
-    #blit the assets to the screen
-    display_surface.blit(player_image, player_rect)
-    display_surface.blit(coin_image, coin_rect)
